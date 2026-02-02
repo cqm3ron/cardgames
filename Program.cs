85e453517f978e22d81b6e53859c3a8362a2da43
cqm3ron <73529986+cqm3ron@users.noreply.github.com>
2026-02-02 12:57:11 +0000

    Add files via upload
    Add an optional extended description...

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using cardgames.accounts;
+using cardgames.core;
+using cardgames.games.blackjack;
+
+namespace cardgames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountManager accountManager = new AccountManager();
+            User user = new User();
+            
+
+            //BlackjackGame game = new();
+        }
+    }
+}
---
+++ games/blackjack/Dealer.cs
@@ -0,0 +1,52 @@
+﻿using cardgames.core;
+using System.Numerics;
+
+namespace cardgames.games.blackjack
+{
+    internal class Dealer
+    {
+        public bool bust { get; private set; }
+        private List<Card> hand { get; } = [];
+        
+        public void AddCardToHand(Card card) => hand.Add(card);
+        public int GetHandValue()
+        {
+            int total = 0;
+            int aces = 0;
+            foreach (Card card in hand)
+            {
+                if (card.GetRank() == Card.Rank.ace)
+                {
+                    aces++;
+                    total += 11;
+                }
+                else
+                {
+                    total += card.GetBlackjackValue();
+                }
+                while (total > 21 && aces > 0)
+                {
+                    total -= 10;
+                    aces--;
+                }
+            }
+            return total;
+        }
+        public Card GetPublicCard()
+        {
+            return hand[0];
+        }
+        public void Draw(Deck deck)
+        {
+            while (GetHandValue() < 17)
+            {
+                AddCardToHand(deck.Draw());
+            }
+
+            if (GetHandValue() > 21)
+            {
+                bust = true;
+            }
+        }
+    }
+}
